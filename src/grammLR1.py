3087088a60d7bd4ee1623a376dda0529d7245aa3
Eric <eric.tornas@gmail.com>
2024-03-26 19:08:40 -0600

    cambio

--- src/grammLR1.py
+++ src/grammLR1.py
@@ -5,9 +5,9 @@ def gramm_Hulk_LR1():
     Program = G.NonTerminal('Program', True)
     statement_list, statement, condition, expression, term, factor, function_call, arguments, parameters = G.NonTerminals('statement_list statement condition expression term factor function_call arguments parameters')
     type_definition, attribute_definition, method_definition, inheritance, instance_creation, member_access, type_annotation = G.NonTerminals('type_definition attribute_definition method_definition inheritance instance_creation member_access type_annotation')
-    print_statement, assignment, function_definition, control_structure, contElif, contElse = G.NonTerminals('print_statement assignment function_definition control_structure contElif contElse')
+    print_statement, assignment, function_definition, control_structure, contElif, contElse, exp_or_cond= G.NonTerminals('print_statement assignment function_definition control_structure contElif contElse exp_or_cond')
     if_structure, while_structure, for_structure, member,method_override, create_statement, non_create_statement = G.NonTerminals('if_structure while_structure for_structure member method_override create_statement non_create_statement')
-    compAritCond, compBoolCond, base_args, let_in, simple_assignment, multi_assignment, kern_assignment  = G.NonTerminals('compAritCond compBoolCond base_args let_in simple_assignment multi_assignment kern_assignment')
+    compAritCond, compBoolCond, base_args, let_in, multi_assignment, kern_assignment, op_factor, op_term  = G.NonTerminals('compAritCond compBoolCond base_args let_in multi_assignment kern_assignment op_factor op_term')
     
     Print, oPar, cPar, oBrace, cBrace, Semi, Equal, Plus, Minus, Mult, Div, Arrow, Mod = G.Terminals('print ( ) { } ; = + - * / => %')
     And, Or, Not, Less, Greater, Equal, LessEqual, GreaterEqual, NotEqual, Is, In, _True, _False = G.Terminals('and or not < > == >= <= != is in True False')
@@ -36,12 +36,14 @@ def gramm_Hulk_LR1():
     while_structure %= While + oPar + condition + cPar + oBrace + statement_list + cBrace 
     for_structure %= For + oPar + assignment + Semi + condition + Semi + assignment + cPar + oBrace + statement_list + cBrace
     
-    condition %= expression + compAritCond + expression | Not + condition | condition + compBoolCond + condition | _True | _False | expression + Is + def_Type | oPar + condition + cPar | function_call | identifier
+    condition %= expression + compAritCond + expression | Not + condition | condition + compBoolCond + condition | _True | _False | identifier + Is + def_Type | oPar + condition + cPar | function_call | identifier
     compAritCond %= Less | Greater | Equal | LessEqual | GreaterEqual | NotEqual 
     compBoolCond %= And | Or 
-    expression %= term + Plus + term | term + Minus + term | term 
-    term %= factor + Mult + factor | factor + Div + factor | factor + Mod + factor | factor
-    factor %= number | string | oPar + expression + cPar | function_call | identifier
+    expression %= term + op_term + term | term 
+    op_term %= Plus | Minus
+    term %= factor + op_factor + factor | factor
+    op_factor %= Mult | Div | Mod
+    ##factor %= number | string | oPar + expression + cPar | function_call | identifier | member_access
     
     function_call %= identifier + oPar + arguments + cPar
     base_args %= expression | condition | G.Epsilon
@@ -51,14 +53,15 @@ def gramm_Hulk_LR1():
     let_in %= assignment + In + non_create_statement | assignment + In + oBrace + statement_list + cBrace
     
     # Estructuras adicionales para tipos
-    type_definition %= Type + identifier + inheritance + oBrace + attribute_definition + method_definition + cBrace 
-    attribute_definition %= identifier + type_annotation + Equal + expression + Semi + attribute_definition | G.Epsilon
-    method_definition %= identifier + oPar + parameters + cPar + oBrace + expression + cBrace + method_definition | G.Epsilon
+    ##type_definition %= Type + identifier + inheritance + oBrace + attribute_definition + method_definition + cBrace 
+    exp_or_cond %= expression | condition
+    attribute_definition %= identifier + type_annotation + Equal + exp_or_cond + Semi + attribute_definition | G.Epsilon
+    method_definition %= identifier + oPar + parameters + cPar + oBrace + statement_list + cBrace + method_definition | G.Epsilon
     inheritance %= Inherits + def_Type | G.Epsilon
     # Instanciación de tipos
     instance_creation %= Let + identifier + Equal + New + def_Type + oPar + arguments + cPar + Semi
-    
-    method_override %= identifier + oPar + parameters + cPar + oBrace + expression + Semi + cBrace + method_override | G.Epsilon
+    #
+    method_override %= identifier + oPar + parameters + cPar + oBrace + statement_list + Semi + cBrace | G.Epsilon
     member %= identifier + oPar + arguments + cPar | identifier   
-    member_access %= identifier + Dot + member + Semi
+    member_access %= identifier + Dot + member
     return G
